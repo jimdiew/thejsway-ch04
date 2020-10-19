3b517cb33e3a07a73655b9780c39b055ca0a5843
Jim Williams <39556832+jimdiew@users.noreply.github.com>
2020-10-19 18:59:39 -0300

    initial commit

---
+++ exercise.js
@@ -0,0 +1,16 @@
+let number = 1;
+while (number <= 100) {
+  const divisibleBy3 = number % 3 === 0;
+const divisibleBy5 = number % 5 === 0;
+  
+if (divisibleBy3 && divisibleBy5) {
+  console.log("FizzBizz");
+} else if (divisibleBy3){
+  console.log("Fizz");
+} else if(divisibleBy5) {
+  console.log("Bizz");
+} else {
+  console.log(number);
+}
+  number++;
+  }
